e366ad4295c5420fe4fb59b7dafeec8bf098ab4a
MaRisa Jackson <mhljackson@gmail.com>
2014-10-31 09:49:57 -0500

    Add engine setting to app.js

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'))
    .use(cookieParser());
+app.set('view engine', 'jade');
 
 app.get('/', function(req, res){
   res.render('index');
